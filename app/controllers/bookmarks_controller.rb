72a41b438283a94880cb5675c740d88097c64127
Kylan Metzner <kylan.metzner@audencia.com>
2024-02-23 11:24:09 +0100

    push all for heroku

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,21 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(params_bookmark)
+    @bookmark.list = @list
+    if @bookmark.save!
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def params_bookmark
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
---
+++ db/migrate/20240222104014_modify_list_comment_to_name.rb
@@ -0,0 +1,5 @@
+class ModifyListCommentToName < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :lists, :comment, :name
+  end
+end
